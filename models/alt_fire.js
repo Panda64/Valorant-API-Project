08e578398dd50933d5361309d769202700707827
Panda64 <JaylenSchelb@yahoo.com>
2021-10-06 13:02:11 -0700

    Import missing 'Populate' function to new model files

--- models/alt_fire.js
+++ models/alt_fire.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+const Populate = require("../util/autopopulate")
 
 const AltFireSchema = new Schema({
     type : { type: String, required: true },
--- models/damage_range.js
+++ models/damage_range.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
 const Schema = mongoose.Schema
+const Populate = require("../util/autopopulate")
 
 const DamageRangeSchema = new Schema({
     range : { type: Schema.Types.ObjectId, ref: "RangeValue", required: true },
