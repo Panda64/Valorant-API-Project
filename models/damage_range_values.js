b7b9b917dc4495937806b9e8b6145d64746f7305
Panda64 <JaylenSchelb@yahoo.com>
2021-10-06 22:32:53 -0700

    Add 'special' entry to damage range
    This is to describe the special information of the shotguns

--- models/damage_range_values.js
+++ models/damage_range_values.js
@@ -3,7 +3,8 @@ const Schema = mongoose.Schema
 
 const RangeValueSchema = new Schema({
     low : { type: Number, required: true },
-    high : { type: Number, required: true }
+    high : { type: Number, required: true },
+    special : { type: String }
   })
 
   module.exports = mongoose.model("RangeValue", RangeValueSchema)
