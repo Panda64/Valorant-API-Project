ce9ec5d940a00d5c5e3d6c2ec7768a2864a5b63f
Panda64 <JaylenSchelb@yahoo.com>
2021-10-06 20:53:43 -0700

    alt_fire array attributes now become undefined by default

--- models/alt_fire_attributes.js
+++ models/alt_fire_attributes.js
@@ -4,12 +4,12 @@ const Schema = mongoose.Schema
 const AltFireAttributeSchema = new Schema({
     pellet_count : { type: Number},
     burst_rate : { type: Number},
-    fire_rate : { type: Array},
+    fire_rate : { type: Array, default: void 0 },
     zoom : { type: Number},
-    move_speed : { type: Array},
+    move_speed : { type: Array, default: void 0 },
     rounds_per_burst : { type: Number},
     canister_detonation_distance : { type: Number},
-    special : { type: Array}
+    special : { type: Array, default: void 0 }
   })
 
   module.exports = mongoose.model("AltFireAttribute", AltFireAttributeSchema)
